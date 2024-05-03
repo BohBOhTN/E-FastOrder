8f89957a6f8f35436d6878ef4bbc61730a6e0051
BohBOhTN <90150981+BohBOhTN@users.noreply.github.com>
2024-05-03 01:39:00 +0100

    first commit

---
+++ E-FastOrder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_FastOrder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
